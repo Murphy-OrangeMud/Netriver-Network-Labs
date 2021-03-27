e78c2a2aad538da558ca9099c73ad9a7fa9a8077
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-03-27 11:52:38 +0800

    lab1 passed all tests

---
+++ lab1/sliding_window.cpp
@@ -0,0 +1,180 @@
+// #include "sysinclude.h"
+#include <arpa/inet.h>
+#include <iostream>
+#include <cstdio>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+
+extern void SendFRAMEPacket(unsigned char* pData, unsigned int len);
+
+typedef char uint_8;
+typedef enum { data, ack, nak } frame_kind;
+enum { MSG_TYPE_TIMEOUT, MSG_TYPE_SEND, MSG_TYPE_RECEIVE };
+
+struct frame_head {
+    frame_kind kind;
+    unsigned int seq;
+    unsigned int ack;
+    unsigned char data[100];
+};
+
+struct frame {
+    frame_head head;
+    int size;
+};
+
+struct store_frame {
+    frame f;
+    unsigned int size;
+    store_frame() {}
+    store_frame(frame _frame, int _size): f(_frame), size(_size) {}
+};
+
+int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, uint_8 messageType) {
+    static vector<store_frame> frame_list;
+    static int expect = 0;
+    switch (messageType) {
+        case MSG_TYPE_TIMEOUT: {
+            uint32_t timeout_seq = *(uint32_t *)(pBuffer);
+            store_frame send_frame = frame_list[expect];
+            printf("Timeout, resend %d\n", timeout_seq);
+            SendFRAMEPacket((unsigned char *)(&(send_frame.f)), send_frame.size);
+            break;
+        }
+        case MSG_TYPE_RECEIVE: {
+            store_frame send_frame = frame_list[expect];
+            frame ack_frame = *(frame *)(pBuffer);
+            printf("Receive ack %d\n", ntohl(ack_frame.head.ack));
+            while (send_frame.f.head.seq <= ack_frame.head.ack) {
+                expect++;
+                send_frame = frame_list[expect];
+            }
+            printf("Send a new frame %d\n", ntohl(send_frame.f.head.seq));
+            SendFRAMEPacket((unsigned char *)&(send_frame.f), send_frame.size);
+            break;
+        }
+        case MSG_TYPE_SEND: {
+            //需要发送这一个帧
+            frame *send_frame = (frame *)(pBuffer);
+            printf("Frame info: %d,%d\n", ntohl(send_frame->head.seq), send_frame->head.data);
+            frame_list.push_back(store_frame(*send_frame, bufferSize));
+            if (expect == frame_list.size() - 1) {
+                printf("Sending frame %d...\n", ntohl(send_frame->head.seq));
+                SendFRAMEPacket((unsigned char *)send_frame, bufferSize);
+            } else {
+                printf("Waiting frame %d...\n", ntohl(send_frame->head.seq));
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
+int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, uint_8 messageType) {
+    static const int window_size = 4;
+    static vector<store_frame> frame_list;
+    static int window_begin, window_end;
+    
+    switch (messageType) {
+        case MSG_TYPE_TIMEOUT: {
+            uint32_t timeout_seq = *(uint32_t *)(pBuffer);
+            printf("Timeout seq: %d\n", timeout_seq);
+            for (int i = window_begin; i < window_end; i++) {
+                printf("Timeout, resend %d\n", ntohl(frame_list[i].f.head.seq));
+                SendFRAMEPacket((unsigned char *)(&(frame_list[i].f)), frame_list[i].size);
+            }
+            break;
+        }
+        case MSG_TYPE_RECEIVE: {
+            frame ack_frame = *(frame *)(pBuffer);
+            for (int i = window_begin; i < window_end; i++) {
+                if (frame_list[i].f.head.seq == ack_frame.head.ack) {
+                    for (int j = window_end; j < i + 1 + window_size, j < frame_list.size(); j++) {
+                        printf("Opening window, sending %d\n", ntohl(frame_list[j].f.head.seq));
+                        SendFRAMEPacket((unsigned char *)(&(frame_list[j].f)), frame_list[j].size);
+                    }
+                    window_begin = i + 1;
+                    window_end = min((int)frame_list.size(), window_begin + window_size);
+                    break;
+                }
+            }
+            break;
+        }
+        case MSG_TYPE_SEND: {
+            frame *send_frame = (frame *)(pBuffer);
+            frame_list.push_back(store_frame(*send_frame, bufferSize));
+            if (frame_list.size() <= window_size) {
+                window_end++;
+                printf("Sending frame %d...\n", ntohl(send_frame->head.seq));
+                SendFRAMEPacket((unsigned char *)send_frame, bufferSize);
+            } else {
+                printf("Waiting frame %d...\n", ntohl(send_frame->head.seq));
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
+int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, uint_8 messageType) {
+    static const int window_size = 4;
+    static vector<store_frame> frame_list;
+    static int window_begin, window_end;
+
+    switch (messageType) {
+        case MSG_TYPE_TIMEOUT: {
+            uint32_t timeout_seq = *(uint32_t *)(pBuffer);
+            printf("Timeout seq: %d\n", timeout_seq);
+            for (int i = window_begin; i < window_end; i++) {
+                printf("Timeout, resend %d\n", ntohl(frame_list[i].f.head.seq));
+                SendFRAMEPacket((unsigned char *)(&(frame_list[i].f)), frame_list[i].size);
+            }
+            break;
+        }
+        case MSG_TYPE_RECEIVE: {
+            frame ack_frame = *(frame *)(pBuffer);
+            if (ntohl(ack_frame.head.kind) == ack) {
+                printf("Receive ack %d\n", ntohl(ack_frame.head.ack));
+                for (int i = window_begin; i < window_end; i++) {
+                    if (frame_list[i].f.head.seq == ack_frame.head.ack) {
+                        for (int j = window_end; j < i + 1 + window_size, j < frame_list.size(); j++) {
+                            printf("Opening window, sending %d\n", ntohl(frame_list[j].f.head.seq));
+                            SendFRAMEPacket((unsigned char *)(&(frame_list[j].f)), frame_list[j].size);
+                        }
+                        window_begin = i + 1;
+                        window_end = min((int)frame_list.size(), window_begin + window_size);
+                        break;
+                    }
+                }
+            }
+            else if (ntohl(ack_frame.head.kind) == nak) {
+                printf("Receive ack %d\n", ntohl(ack_frame.head.ack));
+                for (int i = window_begin; i < window_end; i++) {
+                    if (frame_list[i].f.head.seq == ack_frame.head.ack) {
+                        printf("Error, resend %d\n", ntohl(ack_frame.head.ack));
+                        SendFRAMEPacket((unsigned char *)(&(frame_list[i].f)), frame_list[i].size);
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        case MSG_TYPE_SEND: {
+            frame *send_frame = (frame *)(pBuffer);
+            frame_list.push_back(store_frame(*send_frame, bufferSize));
+            if (frame_list.size() <= window_size) {
+                window_end++;
+                printf("Sending frame %d...\n", ntohl(send_frame->head.seq));
+                SendFRAMEPacket((unsigned char *)send_frame, bufferSize);
+            } else {
+                printf("Waiting frame %d...\n", ntohl(send_frame->head.seq));
+            }
+            break;
+        }
+    }
+    return 0;
+}
---
+++ lab2/ip_receive_send.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstdio>
+#include <arpa/inet.h>
+#include <random>
+// #include "sysinclude.h"
+using namespace std;
+
+extern void ip_DiscardPkt(char *pBuffer, int type);
+extern void ip_SendtoLower(char *pBuffer, int length);
+extern void ip_SendtoUp(char *pBuffer, int length);
+extern unsigned int getIpv4Address();
+
+enum {
+    STUD_IP_TEST_CHECKSUM_ERROR,
+    STUD_IP_TEST_TTL_ERROR,
+    STUD_IP_TEST_VERSION_ERROR,
+    STUD_IP_TEST_HEADLEN_ERROR,
+    STUD_IP_TEST_DESTINATION_ERROR
+};
+
+struct ipv4_head {
+    unsigned short version_IHL_service;
+    unsigned short total_length;
+    unsigned short identification;
+    unsigned short fragment_offset;
+    unsigned short TTL_protocol;
+    unsigned short header_checksum;
+    unsigned long source_address;
+    unsigned long destination_address;
+};
+
+typedef unsigned char byte;
+
+int stud_ip_recv(char *pBuffer, unsigned short length) {
+    ipv4_head *recv_head = (ipv4_head *)(pBuffer);
+    short version = (recv_head->version_IHL_service & 0xf000) >> 12;
+    short ttl = (recv_head->TTL_protocol & 0xff) >> 8;
+    short head_len = (recv_head->version_IHL_service & 0x0f00) >> 8;
+    unsigned int sum = 0;
+    // 按大端顺序加
+    sum += ~(recv_head->version_IHL_service);
+    sum += ~(recv_head->total_length);
+    sum += ~(recv_head->identification);
+    sum += ~(recv_head->fragment_offset);
+    sum += ~(recv_head->TTL_protocol);
+    sum += ~(recv_head->header_checksum);
+    sum += ~(recv_head->source_address & 0xffff);
+    sum += ~((recv_head->source_address & 0xffff0000) >> 16);
+    sum += ~(recv_head->destination_address & 0xffff);
+    sum += ~((recv_head->destination_address & 0xffff0000) >> 16);
+    sum = ((sum & 0xffff) + ((sum & 0xffff0000) >> 16)) & 0xffff;
+
+    if (version != 4) {
+        ip_DiscardPkt(pBuffer, STUD_IP_TEST_VERSION_ERROR);
+        return 1;
+    }
+    
+    if (ntohs(recv_head->total_length) != length) {
+        ip_DiscardPkt(pBuffer, STUD_IP_TEST_HEADLEN_ERROR);
+        return 1;
+    }
+
+    if (ttl == 0) {
+        ip_DiscardPkt(pBuffer, STUD_IP_TEST_TTL_ERROR);
+        return 1;
+    }
+
+    if (sum != 0) {
+        ip_DiscardPkt(pBuffer, STUD_IP_TEST_CHECKSUM_ERROR);
+        return 1;
+    }
+
+    if (ntohl(recv_head->destination_address) != getIpv4Address()) {
+        ip_DiscardPkt(pBuffer, STUD_IP_TEST_DESTINATION_ERROR);
+        return 1;
+    }
+
+    char *data = pBuffer + head_len;
+    ip_SendtoUp(data, length - head_len);
+
+    return 0;
+}
+
+int stud_ip_Upsend(char *pBuffer, unsigned short len, unsigned int srcAddr, unsigned int dstAddr, byte protocol, byte ttl) {
+    char *send_head = (char *)malloc(len + sizeof(ipv4_head));
+    ((ipv4_head *) send_head)->source_address = htonl(srcAddr);
+    ((ipv4_head *) send_head)->destination_address = htonl(dstAddr);
+    ((ipv4_head *) send_head)->TTL_protocol = (((unsigned short) ttl) << 4) | ((unsigned short) protocol);
+    ((ipv4_head *) send_head)->total_length = htons(len + sizeof(ipv4_head));
+    ((ipv4_head *) send_head)->identification = (unsigned short) random();
+    ((ipv4_head *) send_head)->fragment_offset = 0x4000;
+    ((ipv4_head *) send_head)->version_IHL_service = (0x4000) | ((sizeof(ipv4_head)) << 8);
+
+    unsigned int sum = 0;
+    sum += ~(((ipv4_head *) send_head)->version_IHL_service);
+    sum += ~(((ipv4_head *) send_head)->total_length);
+    sum += ~(((ipv4_head *) send_head)->identification);
+    sum += ~(((ipv4_head *) send_head)->fragment_offset);
+    sum += ~(((ipv4_head *) send_head)->TTL_protocol);
+    sum += ~(((ipv4_head *) send_head)->header_checksum);
+    sum += ~(((ipv4_head *) send_head)->source_address & 0xffff);
+    sum += ~((((ipv4_head *) send_head)->source_address & 0xffff0000) >> 16);
+    sum += ~(((ipv4_head *) send_head)->destination_address & 0xffff);
+    sum += ~((((ipv4_head *) send_head)->destination_address & 0xffff0000) >> 16);
+    sum = ((sum & 0xffff) + ((sum & 0xffff0000) >> 16)) & 0xffff;
+    
+    ((ipv4_head *) send_head)->header_checksum = (unsigned short) sum;
+    ip_SendtoLower(send_head, len + sizeof(ipv4_head));
+    return 0;
+}
